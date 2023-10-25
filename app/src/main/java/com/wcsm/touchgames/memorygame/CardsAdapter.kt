9429b2f715b5749904af6be7a6c10387bd38387b
Wallace Martins <wallace159santos@hotmail.com>
2023-10-24 23:31:41 -0300

    implemented 2 players game mode

--- app/src/main/java/com/wcsm/touchgames/memorygame/CardsAdapter.kt
+++ app/src/main/java/com/wcsm/touchgames/memorygame/CardsAdapter.kt
@@ -8,9 +8,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.wcsm.touchgames.R
+
+enum class PlayerTurn{
+    PLAYER1, PLAYER2
+}
 
 class CardsAdapter(
     private val list: MutableList<Card>,
@@ -29,14 +34,25 @@ class CardsAdapter(
 
     private var previousItemView: View? = null
 
+    // Singleplayer Variables
     private var singleplayerPoints = 0
     private var countUpTimer: CountUpTimer? = null
+
+    // Two Players Game Variables
+    private var firstPlayerPoints = 0
+    private var secondPlayerPoints = 0
+    private var turn = PlayerTurn.PLAYER1
 
     init {
         countUpTimer = object : CountUpTimer(1000) {
             override fun onTick(elapsedTime: Long) {
                 updateTimerUI(elapsedTime)
             }
+        }
+
+        if(gameType == MemoryGameGameTypes.TWOPLAYERS) {
+            textView1.text = "1º Jogador: $firstPlayerPoints"
+            textView2.text = "2º Jogador: $secondPlayerPoints"
         }
     }
 
@@ -48,8 +64,11 @@ class CardsAdapter(
 
         private val cardDefault: ImageView = itemView.findViewById(R.id.mg_card_default)
 
-        fun bind(card: Card) {
+        private var playerTurnColor = ContextCompat.getColor(itemView.context, R.color.mg_player_turn)
+        private var playerDefaultColor = ContextCompat.getColor(itemView.context, R.color.black)
 
+        fun bind(card: Card) {
+            textView1.setTextColor(playerTurnColor)
             cardDefault.setImageResource(R.drawable.mg_card_back) // código para mostrar os quadrados (carta pra baixo)
             //cardDefault.setImageResource(card.imageSrc) // código para mostrar os cards
 
@@ -79,14 +98,22 @@ class CardsAdapter(
                         cardsMatched = true
 
                         Log.i("MEMORY_GAME", "COMBINOU AS CARTAS")
-                        singleplayerPoints = checkPontuation(Operations.PLUS, singleplayerPoints)
 
                         selectedCard = initialCard
                         plays = 0
 
                         // Pontuation and Time Manipulation
                         if(gameType == MemoryGameGameTypes.SINGLEPLAYER) {
+                            singleplayerPoints = checkPontuation(Operations.PLUS, singleplayerPoints)
                             textView1.text = "Pontos: $singleplayerPoints"
+                        } else if(gameType == MemoryGameGameTypes.TWOPLAYERS) {
+                            if(turn == PlayerTurn.PLAYER1) {
+                                firstPlayerPoints = checkPontuation(Operations.PLUS, firstPlayerPoints)
+                                textView1.text = "Jogador 1: $firstPlayerPoints"
+                            } else if(turn == PlayerTurn.PLAYER2) {
+                                secondPlayerPoints = checkPontuation(Operations.PLUS, secondPlayerPoints)
+                                textView2.text = "Jogador 2: $secondPlayerPoints"
+                            }
                         }
                         itemView.isEnabled = true
                     }, 1000)
@@ -114,12 +141,25 @@ class CardsAdapter(
 
                             previousItemView = null
 
-                            singleplayerPoints = checkPontuation(Operations.MINUS, singleplayerPoints)
-
                             // Losing Points if cards don't combine
                             if(gameType == MemoryGameGameTypes.SINGLEPLAYER) {
+                                singleplayerPoints = checkPontuation(Operations.MINUS, singleplayerPoints)
                                 textView1.text = "Pontos: ${singleplayerPoints}"
-                                Log.i("MEMORY_GAME", "APÓS textView1.text: ${textView1.text}")
+                            } else if(gameType == MemoryGameGameTypes.TWOPLAYERS) {
+                                Log.i("MEMORY_GAME", "ENTROU: else if(gameType == MemoryGameGameTypes.TWOPLAYERS) | turn: $turn")
+                                if(turn == PlayerTurn.PLAYER1) {
+                                    Log.i("MEMORY_GAME", "ENTROU: if(turn == PlayerTurn.PLAYER2)")
+                                    firstPlayerPoints = checkPontuation(Operations.MINUS, firstPlayerPoints)
+                                    textView1.text = "Jogador 1: $firstPlayerPoints"
+                                    turn = PlayerTurn.PLAYER2
+                                    changeTurnColors(turn, playerTurnColor, playerDefaultColor)
+                                } else if(turn == PlayerTurn.PLAYER2) {
+                                    Log.i("MEMORY_GAME", "ENTROU: else if(turn == PlayerTurn.PLAYER2)")
+                                    secondPlayerPoints = checkPontuation(Operations.MINUS, secondPlayerPoints)
+                                    textView2.text = "Jogador 2: $secondPlayerPoints"
+                                    turn = PlayerTurn.PLAYER1
+                                    changeTurnColors(turn, playerTurnColor, playerDefaultColor)
+                                }
                             }
 
                             itemView.isEnabled = true
@@ -178,5 +218,15 @@ class CardsAdapter(
         val minutes = (elapsedTime / 1000) / 60
         val seconds = (elapsedTime / 1000) % 60
         textView2.text = String.format("%02d:%02d", minutes, seconds)
+    }
+
+    fun changeTurnColors(newTurn: PlayerTurn, turnColor: Int, defaultColor: Int) {
+        if(newTurn == PlayerTurn.PLAYER1) {
+            textView2.setTextColor(defaultColor)
+            textView1.setTextColor(turnColor)
+        } else if(newTurn == PlayerTurn.PLAYER2) {
+            textView1.setTextColor(defaultColor)
+            textView2.setTextColor(turnColor)
+        }
     }
 }
